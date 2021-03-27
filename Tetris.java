953029cfb216e46a5698e689ab0191389acb8953
Alexey Tarabanov <tarabanov@bk.ru>
2021-03-27 23:19:30 +0300

    2/17

--- Tetris.java
+++ Tetris.java
@@ -4,6 +4,12 @@ package com.javarush.task.task22.task2213;
  * Главный класс.
  */
 public class Tetris {
+
+    // поле с клетками
+    private Field field;
+    // фигуры
+    private Figure figure;
+
     public static void main(String[] args) {
 
     }
