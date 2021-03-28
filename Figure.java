eabad8dfec4374e7ea18cc27a2b3ae8cb3150eb3
Alexey Tarabanov <tarabanov@bk.ru>
2021-03-28 18:50:32 +0300

    8/17

--- Figure.java
+++ Figure.java
@@ -2,6 +2,31 @@ package com.javarush.task.task22.task2213;
 
 /**
  * Класс отвечающий за фигуры.
+ * Будет описывать падающую фигурку.
  */
 public class Figure {
+
+    // координаты
+    private int x;
+    private int y;
+    // форма объекта
+    private int[][] matrix;
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
+
+    public Figure(int x, int y, int[][] matrix) {
+        this.x = x;
+        this.y = y;
+        this.matrix = matrix;
+    }
 }
