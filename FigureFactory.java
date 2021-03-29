56ffe8e04cab33861835c95764655580cbdebfc8
Alexey Tarabanov <tarabanov@bk.ru>
2021-03-29 22:28:49 +0300

    11/17

---
+++ FigureFactory.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task22.task2213;
+
+/**
+ * С помощью этого класса будем создавать фигуры различных форм.
+ */
+public class FigureFactory {
+
+    public static Figure createRandomFigure(int x,int y) {
+        return null;
+    }
+}
